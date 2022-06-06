d6256214ad1637b7eff8d658d14a62d223886389
Alexis Philip George-Georganopoulos <georganopoulos.alexis@gmail.com>
2022-06-06 11:09:22 +0200

    cleaned up the octagon generator so it can be run anywhere

--- octagon/octagon_generator.py
+++ octagon/octagon_generator.py
@@ -9,16 +9,16 @@ Created on Tue Apr 13 09:38:30 2021
 from os import path
 import numpy as np
 from PIL import Image
-#C:\Users\User\Documents\EPFL\Robotics-2019_2020-Year 3\Masters Thesis\
-image_source = path.dirname(__file__)
 
+image_source = path.dirname(__file__) 
 image_file = Image.open(image_source+'\octagon-deluxe-spa-cover-50.png') # open colour image
+
 thresh = 200
 fn = lambda x : 255 if x > thresh else 0
 image_file = image_file.convert('L').point(fn, mode='1')
 
 
-image_file.show()
+#image_file.show()
 
 mtrx = np.array(image_file)
 W,L= np.shape(mtrx)[0],np.shape(mtrx)[1]
@@ -47,20 +47,29 @@ for i in range(W):
             break
         
 image_file = image_file.crop((left,top,right,bottom))
-image_file.show()
+#image_file.show()
 mtrx = np.array(image_file)
 
 newsize = (101,101)
 image_file = image_file.resize(newsize)
 
-docs_dir=os.path.expanduser('~\Documents\EPFL\Robotics-2019_2020-Year 3\Masters Thesis')
-image_file.save(os.path.join(docs_dir,'OCTAGON.png'))
+#save as png
+image_file.save(path.join(image_source,'OCTAGON.png'))
 
 image_file.show()
 mtrx = np.array(image_file)
 mtrx = (~mtrx)*1
 
+#save as txt
+np.savetxt(path.join(image_source,'OCTAGON.txt'),mtrx, fmt='%d',delimiter='')
 
-np.savetxt(os.path.join(docs_dir,'OCTAGON.txt'),mtrx, fmt='%d',delimiter='')
 del i
 del fn
+
+#save as ima
+np.savetxt(path.join(image_source,'OCTAGON.IMA'),mtrx, fmt='%d',delimiter='')
+with open("OCTAGON.IMA", "r+") as f:
+     old = f.read() # read everything in the file
+     f.seek(0) # rewind
+     f.write("101\n" + old) # write the new line before
+     f.close()
